52d5cb3c4987f6a9c3cd037a55a5e2fefc8afb1f
Thomas Ikimi <themerc01@gmail.com>
2017-10-24 17:31:05 +0100

    api Call connected to Article List connecting

--- src/apiCall.js
+++ src/apiCall.js
@@ -3,19 +3,32 @@
   var apiCall = new apiCall();
 
   function apiCall() {
+    // this.apiList = new ArticleList()
+    // this.getApi()
+console.log(this.apiArticles)
+
+// apiCall.prototype.getApi = function(){
 
     var httpRequest = new XMLHttpRequest();
     var url = "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=c013df4cb4354f4ea56e3a7e16373797"
     httpRequest.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var data = httpRequest.responseText
-          var parseddata = JSON.parse(data);
-          var contentArray = JSON.stringify(parseddata)
-          document.getElementById('articles').innerHTML = contentArray
+            console.log('1', data)
+          var parseddata = JSON.parse(data).articles;
+          console.log('2', parseddata)
+          // var contentArray = JSON.stringify(parseddata)
+          this.apiArticles = parseddata
+          apiList = new ArticleList()
+          apiList.getArticles(parseddata))
+          // this.apiList.push(parseddata)
+          console.log('4', parseddata)
+          console.log('5', this.apiArticles)
         }
     };
     httpRequest.open("GET", url, true);
     httpRequest.send();
   }
+// }
   exports.apiCall = apiCall;
 }) (this);
