67d55532c6d94f891d1ea6d1bb44070e7cd6cf82
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-26 21:17:40 +0100

    Added MatchWords function to filter headlines for an array of keywords, and updated specrunner.html

---
+++ spec/articles-spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Articles', function() {
+
+  var articles;
+
+  beforeEach(function() {
+    articles = new Articles();
+  });
+
+
+});
---
+++ spec/match-words-spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('MatchWords', function() {
+
+  var matchWords;
+
+  beforeEach(function() {
+    matchWords = new MatchWords();
+  });
+
+});
